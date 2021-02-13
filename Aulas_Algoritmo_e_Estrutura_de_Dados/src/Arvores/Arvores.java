db68ae948d1a00734b06d5333b099b6b0da24d60
Uerviton Santos <uerviton@gmail.com>
2021-02-13 16:16:14 -0300

    commit

--- Aulas_Algoritmo_e_Estrutura_de_Dados/src/Arvores/Arvores.java
+++ Aulas_Algoritmo_e_Estrutura_de_Dados/src/Arvores/Arvores.java
@@ -3,8 +3,21 @@ package Arvores;
 public class Arvores {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		double[] temperatura = new double[365];
+		
+		for (int i = 0; i < temperatura.length; i++) {
+			int rnd = (int) (1 + Math.random() *45);
+			temperatura[i] = rnd;
+			
+		}
+		
+		for (int i = 0; i < temperatura.length; i++) {
+			System.out.println("A temperatura média verificada no dia " + (i+1) + " foi: " + temperatura[i]);
+			
+		}
+		
+		System.out.println("Quantidades de dias avliados: " + temperatura.length);
 	}
 
 }
