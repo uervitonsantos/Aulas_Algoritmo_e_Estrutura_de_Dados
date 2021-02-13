8d2f1b6efd99a57b571b795fe0a20541a75e89f8
Uerviton Santos <uerviton@gmail.com>
2021-02-13 16:34:04 -0300

    commit

--- Aulas_Algoritmo_e_Estrutura_de_Dados/src/Arvores/Arvores.java
+++ Aulas_Algoritmo_e_Estrutura_de_Dados/src/Arvores/Arvores.java
@@ -1,23 +1,10 @@
 package Arvores;
 
+
 public class Arvores {
 
 	public static void main(String[] args) {
 		
-		double[] temperatura = new double[365];
-		
-		for (int i = 0; i < temperatura.length; i++) {
-			int rnd = (int) (1 + Math.random() *45);
-			temperatura[i] = rnd;
-			
-		}
-		
-		for (int i = 0; i < temperatura.length; i++) {
-			System.out.println("A temperatura média verificada no dia " + (i+1) + " foi: " + temperatura[i]);
-			
-		}
-		
-		System.out.println("Quantidades de dias avliados: " + temperatura.length);
 	}
 
 }
--- Aulas_Algoritmo_e_Estrutura_de_Dados/src/Vetores/Temperatura.java
+++ Aulas_Algoritmo_e_Estrutura_de_Dados/src/Vetores/Temperatura.java
@@ -1,10 +1,26 @@
 package Vetores;
 
+//Algoritmo que capitura a temperatura media de 365 dias
+
 public class Temperatura {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+double[] temperatura = new double[365];
+		
+		for (int i = 0; i < temperatura.length; i++) {
+			int rnd = (int) (1 + Math.random() *45);
+			temperatura[i] = rnd;
+			
+		}
+		
+		for (int i = 0; i < temperatura.length; i++) {
+			System.out.println("A temperatura média verificada no dia " + (i+1) + " foi: " + temperatura[i]);
+			
+		}
+		
+		System.out.println("================================================================");
+		
+		System.out.println("Quantidades de dias avliados: " + temperatura.length);
 	}
 
 }
