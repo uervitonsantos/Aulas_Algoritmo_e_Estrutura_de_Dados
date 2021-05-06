4ef86beca8cfab77f6ce50263a194b0d5442357f
uerviton <uerviton@uerviton-santos>
2021-05-05 21:31:45 -0300

    commit

---
+++ Aulas_Algoritmo_e_Estrutura_de_Dados/src/exercicios/orientacao/objeto/TransacaoBancaria.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package exercicios.orientacao.objeto;
+
+import java.util.Calendar;
+
+/**
+ * @author uerviton
+ *
+ */
+public class TransacaoBancaria {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		ContaBancaria carro = new ContaBancaria();
+		
+		carro.setId(1);
+		carro.setAgencia(01);
+		carro.setConta(012345);
+		carro.setSaldo(1985.43);
+		carro.setChequeEspecial(1);
+		carro.setData(Calendar.getInstance().getTime());
+		carro.setClienteAtivo(true);
+
+		System.out.println("Saldo: " + "R$ " + carro.getSaldo());
+		System.out.println("Saldo: " + carro.getData());
+
+	}
+
+}
